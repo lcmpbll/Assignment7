3bc90a4bc9305de342a7d5dfbbcb536a7da89218
Liam Campbell <liam22campbell@gmail.com>
2023-11-05 20:07:20 +0000

    Add GetMaxInt function.

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+/******************************************************************************
+# Author:           Liam Campbell
+# Lab:              Assignment 7
+# Date:             5 November 2023
+# Description:      This program takes data from the user and normalizes it.
+# Input:            int []
+# Output:           int  []
+# Sources:          None
+******************************************************************************/
+
+#include GetMaxInt
+int GetMaxInt(int listInts[20], int numVals) {
+  int largestNumber;
+  // Set largestNumber to first array value
+  largestNumber = listInts[0];
+
+  // Compare largestNumber to each array value
+  for(int i = 0; i < numVals; i++){
+    if(largestNumber < listInts[i]){
+      largestNumber = listInts[i];
+    }
+  }
+  // return largestNumber
+  return largestNumber;
+}
 
 int main(void) {
-  printf("Hello World\n");
+  const int NUM_ELEMENTS = 20;
+  int userValues[NUM_ELEMENTS];
+  int numberOfInputs, normalizeValue;
+  
+
+  scanf("%d", &numberOfInputs);
+  for(int i = 0; i < numberOfInputs; i++){
+    scanf("%d", &userValues[i]);
+  }
+  
+  normalizeValue = GetMaxInt(userValues, numberOfInputs);
+
+  
+  
+  
   return 0;
 }
